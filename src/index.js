26c85dfae3ecb4571c52b05708100cb9d30c8987
Jose <jm.diaz6620@gmail.com>
2024-11-29 19:05:01 +0100

    Working on CORS error

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ setInterval(loadNewsCache, 3600000)
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http:localhost:5173'
+    origin: 'http://localhost:5173'
 }))
 
 app.use('/', newsRouter)
